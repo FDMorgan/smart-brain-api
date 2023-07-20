205191b856c7b68a2eac183ac60d822212441c22
FDMorgan <shadowhawk5111@gmail.com>
2023-07-20 19:57:39 -0400

    updating

--- server.js
+++ server.js
@@ -16,17 +16,6 @@ const db = knex({
 
 const app = express();
 
-const whitelist = ['http://localhost:3001']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if(whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
 app.use(cors())
 app.use(express.json());
 
