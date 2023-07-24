2db26d8f810eb508cb8377283755dfa8856e134e
FDMorgan <shadowhawk5111@gmail.com>
2023-07-23 21:26:54 -0400

    updating

--- server.js
+++ server.js
@@ -11,7 +11,15 @@ const auth = require('./controllers/authorization')
 
 const db = knex({
   client: 'pg',
-    connection: process.env.POSTGRES_URI
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+      host: process.env.DATABASE_HOST,
+      port: 5432,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PW,
+      database: process.env.DATABASE_DB
+    }
 });
 
 const app = express();
