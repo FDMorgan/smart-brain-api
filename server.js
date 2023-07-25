8b8eaa288275f15a2c44c8410b5ffcbd39343b82
FDMorgan <shadowhawk5111@gmail.com>
2023-07-25 08:06:10 -0400

    update

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const auth = require('./controllers/authorization')
 
 const db = knex({
   client: 'pg',
-  connection: process.env.DB_URL
+  connection: {
+    connectionString: process.env.DB_URL,
+    ssl: { rejectUnauthorized: false }
+  }
 });
 
 const app = express();
